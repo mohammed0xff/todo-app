632f8d68b91f8376fc4d3d9a6f6353c569842c5d
mohammed0xff <mohammed0xff@gmail.com>
2023-08-12 15:39:43 +0300

    add `TaskDict` : a dictionary in memory db, holding 10 `Task` instances, to service collection as a singleton.

--- backend/TodoAPI/TodoAPI/Program.cs
+++ backend/TodoAPI/TodoAPI/Program.cs
@@ -1,11 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using Task = TodoAPI.Entities.Task;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Dictionary<int, Task> TaskDict = new();
+
+// adding 10 tasks to our dictionary
+for (int i = 1; i <= 10; i++)
+{
+    var task = new Task($"Task Number : {i}");
+    TaskDict[task.Id] = task;
+}
+
+builder.Services.AddSingleton(TaskDict);
+
 
 var app = builder.Build();
 
