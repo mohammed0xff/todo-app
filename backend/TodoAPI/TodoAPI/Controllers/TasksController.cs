efa184eb9726b5eb77f6483dcb1a056f234556ca
mohammed0xff <mohammed0xff@gmail.com>
2023-08-12 15:31:57 +0300

    add `TasksController` with two endpoints GET `/tasks` & PUT `/toggle-completed/{id}`

---
+++ backend/TodoAPI/TodoAPI/Controllers/TasksController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TodoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly Dictionary<int, Entities.Task> _taskDict;
+        public TasksController(
+            Dictionary<int, Entities.Task> TaskDict
+            )
+        {
+            _taskDict = TaskDict;
+        }
+
+        // GET: api/<TasksController>
+        [HttpGet]
+        public IEnumerable<Entities.Task> Get()
+        {
+            return _taskDict.Values;
+        }
+
+        // PUT api/<TasksController>/5
+        [HttpPut("toggle-completed/{id}")]
+        public IActionResult Put(int id)
+        {
+            if (!_taskDict.ContainsKey(id))
+            {
+                return NotFound();
+            }
+
+            var task = _taskDict[id];
+            task.IsCompleted = !task.IsCompleted;
+
+            return Ok();
+        }
+    }
+}
