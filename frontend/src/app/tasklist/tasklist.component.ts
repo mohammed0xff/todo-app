d8d15af2beddb83fa5187d726568ef3976885014
mohammed0xff <mohammed0xff@gmail.com>
2023-08-12 21:24:14 +0300

    add basic task displayment

--- frontend/src/app/tasklist/tasklist.component.ts
+++ frontend/src/app/tasklist/tasklist.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../constants/Task';
 
 @Component({
   selector: 'app-tasklist',
   templateUrl: './tasklist.component.html',
   styleUrls: ['./tasklist.component.css']
 })
-export class TasklistComponent {
 
+export class TasklistComponent implements OnInit {
+  
+  tasks: Task[] = [{id : 0,  description : "just testing", createdAt : "now", isCompleted : false}];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCheck(index: number) {
+  }
 }
