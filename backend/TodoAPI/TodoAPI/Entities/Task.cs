3ef978f0b20f6d18d355a62895eff58629da139e
mohammed0xff <mohammed0xff@gmail.com>
2023-08-12 15:30:25 +0300

    add `Task` entity

---
+++ backend/TodoAPI/TodoAPI/Entities/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Entities
+{
+    public class Task
+    {
+        public int Id { get; }
+        private static int Next_Id = 0;
+        
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+        public string CreatedAt { get; } = DateTime.Now.ToString("dd/mm/yyyy hh:mm:tt"); // tt for AM/PM
+        public bool IsCompleted { get; set; }
+        
+        public Task(string description)
+        {
+            Id = Next_Id++;
+            Description = description;
+        }
+    }
+}
