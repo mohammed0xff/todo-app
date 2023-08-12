07140f5f7369ee0b74da94ffe033462bc4cf3922
mohammed0xff <mohammed0xff@gmail.com>
2023-08-12 21:47:56 +0300

    feat : edit `TasklistComponent` to list Tasks and enable checking.

--- frontend/src/app/tasklist/tasklist.component.ts
+++ frontend/src/app/tasklist/tasklist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../constants/Task';
+import { TaskService } from '../services/TaskService'
 
 @Component({
   selector: 'app-tasklist',
@@ -9,14 +10,21 @@ import { Task } from '../constants/Task';
 
 export class TasklistComponent implements OnInit {
   
-  tasks: Task[] = [{id : 0,  description : "just testing", createdAt : "now", isCompleted : false}];
+  tasks: Task[] = [];
 
-  constructor() {
+  constructor(private taskService: TaskService) {
   }
 
   ngOnInit(): void {
+    this.taskService.GetTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
   }
 
   onCheck(index: number) {
+    const taskId = this.tasks[index].id;
+    this.taskService.ToggleCompleted(taskId).subscribe(() => {
+      this.tasks[index].isCompleted = !this.tasks[index].isCompleted;
+    });
   }
 }
