c520c1fa2703c1ae16d2749d8fa0e76938320d8a
mohammed0xff <mohammed0xff@gmail.com>
2023-08-15 21:08:39 +0300

    add `TaskList` entity that will hold multiple `Task` entities

---
+++ backend/src/Models/TaskListRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class TaskListRequestModel
+    {
+        [Required]
+        [MaxLength(80)]
+        public string Title { get; set; }
+    }
+}
