8086a13910a439673fdba07c84cec0064865c7c1
mohammed0xff <mohammed0xff@gmail.com>
2023-08-16 16:50:59 +0300

    add pagination element and handler to lists component

--- frontend/src/app/lists/lists.component.ts
+++ frontend/src/app/lists/lists.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PageEvent } from '@angular/material/paginator';
+
 import { SharedDataService  } from '../services/SharedDataService';
 import { TaskListService } from '../services/TaskListService';
 import { TaskList } from '../constants/TaskList';
+
 
 @Component({
   selector: 'app-lists',
@@ -11,8 +13,14 @@ import { TaskList } from '../constants/TaskList';
   styleUrls: ['./lists.component.css'],
 })
 export class ListsComponent {
+  
   lists: TaskList[] = []
   selectedListIdx: number = 0;
+  
+  length :number = 0; 
+  pageIndex :number = 0;
+  pageSize :number = 5;
+  pageEvent: PageEvent | undefined ;
   
   constructor(
     private sharedDataService: SharedDataService, 
@@ -21,17 +29,27 @@ export class ListsComponent {
   }
   
   ngOnInit(): void {
-    this.taskListService.GetLists().subscribe(res => {
-      this.lists = res;
+    this.paginateLists();
+  }
+  
+  paginateLists(){
+    this.taskListService.GetLists(this.pageIndex + 1, this.pageSize).subscribe(res => {
+      this.lists = res.data;
+      this.length = res.totalRecords;
+      this.pageIndex = res.pageNumber - 1;
+      this.pageSize = res.pageSize;
+      
       const firstList = this.lists[0];
       if(firstList === null || firstList === undefined) return;
       this.sharedDataService.setListId(firstList.id);
     })
   }
-  
+
   onSubmit(form: NgForm){
     this.taskListService.AddList(form.value.title).subscribe(res => {
-      this.lists = [...this.lists, res];
+      // go to last page where the new list just made exists
+      this.pageIndex = Math.floor(this.length / this.pageSize ); 
+      this.paginateLists();
       form.reset();
     })
   }
@@ -45,4 +63,11 @@ export class ListsComponent {
   getListStyle(listIdx: number) :string {
     return listIdx === this.selectedListIdx ? "selected" : "";
   }
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.pageIndex = e.pageIndex;
+    this.paginateLists();
+  }
+
 }
